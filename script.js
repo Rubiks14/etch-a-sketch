6e0798913696a96c3eb9c1787ef25c0e830ac297
Derek Thompson <3637169+Rubiks14@users.noreply.github.com>
2026-09-02 16:21:43 -0500

    Add resize button

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 "use strict"
 
-const GRID_WIDTH = 16;
-const GRID_HEIGHT = 16;
+const MIN_GRID_SIZE = 16;
+const MAX_GRID_SIZE = 100;
+const CONTAINER_WIDTH = 720;
+
+let gridSize = 16
+let unitSize = CONTAINER_WIDTH / gridSize;
 
 const container = document.getElementById("container");
+container.style.width = `${CONTAINER_WIDTH}px`
 
-for(let step = 0; step < GRID_WIDTH * GRID_HEIGHT; step++) {
-    const gridPiece = document.createElement("div");
-    gridPiece.classList.toggle("piece");
-    container.appendChild(gridPiece);
+document.querySelector("#resizeButton").addEventListener("click", () => {
+    let size = 0;
+    size = prompt("Please enter a grid size between 16 and 100");
+    if (size < MIN_GRID_SIZE) {
+        size = MIN_GRID_SIZE;
+    }
+    else if (size > MAX_GRID_SIZE) {
+        size = MAX_GRID_SIZE;
+    }
+    else if (size == '') {
+        size = gridSize;
+    }
+    gridSize = size;
+    unitSize = CONTAINER_WIDTH / gridSize;
+    initBoard();
+});
+
+function initBoard() {
+    container.textContent = '';
+    for(let step = 0; step < gridSize * gridSize; step++) {
+        const gridPiece = document.createElement("div");
+        gridPiece.classList.toggle("piece");
+        gridPiece.style.width = `${unitSize}px`;
+        gridPiece.style.height = `${unitSize}px`;
+        gridPiece.addEventListener("pointerenter", function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            if (!this.classList.contains("filled")) {
+                this.classList.add("filled");
+            }
+        }, {once : true});
+        container.appendChild(gridPiece);
+    }
 }
 
-container.style.width = `${GRID_WIDTH*1}em`
-
-for (const piece of container.children) {
-    piece.addEventListener("pointerenter", function(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        if (!this.classList.contains("filled")) {
-            this.classList.add("filled");
-        }
-    }, {once : true});
-}
+initBoard();
